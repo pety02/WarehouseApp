130f4eab622ef9a532fe94d3c6123c26c338dea3
pety02 <pety02@abv.bg>
2025-11-02 19:43:38 +0200

    Swagger included as dependency and used in the get end-point of currencies.

--- src/main/java/com/example/warehouseapp/controller/CurrencyController.java
+++ src/main/java/com/example/warehouseapp/controller/CurrencyController.java
@@ -3,6 +3,9 @@ package com.example.warehouseapp.controller;
 import com.example.warehouseapp.exception.NotFoundEntityException;
 import com.example.warehouseapp.model.dto.CurrencyResponseDTO;
 import com.example.warehouseapp.service.CurrencyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,12 +25,20 @@ public class CurrencyController {
         this.currencyService = currencyService;
     }
 
+    @Operation(summary = "Get a currency by id", description = "Returns a currency as per the id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "400", description = "Bad request - the request was mistaken"),
+            @ApiResponse(responseCode = "404", description = "Not found - The product was not found")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<CurrencyResponseDTO> getCurrencyById(@PathVariable(name = "id") UUID id) {
         CurrencyResponseDTO responseDTO;
 
         try {
              responseDTO = this.currencyService.getCurrencyById(id);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().build();
         } catch (NotFoundEntityException ex) {
             return ResponseEntity.notFound().build();
         }
