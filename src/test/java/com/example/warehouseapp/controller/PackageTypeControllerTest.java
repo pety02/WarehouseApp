a241c4cc4030a387e4187bd5a0a9c231211801a8
pety02 <petya.licheva2002@abv.bg>
2025-12-28 23:14:32 +0200

    Package type controller unit tested.

---
+++ src/test/java/com/example/warehouseapp/controller/PackageTypeControllerTest.java
@@ -0,0 +1,51 @@
+package com.example.warehouseapp.controller;
+
+import com.example.warehouseapp.model.dto.PackageTypeResponseDTO;
+import com.example.warehouseapp.service.PackageTypeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PackageTypeController.class)
+class PackageTypeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private PackageTypeService packageTypeService;
+
+    @Test
+    void getAllPackageTypes_ok() throws Exception {
+        when(packageTypeService.getAllIPackageTypes()).thenReturn(List.of());
+
+        mockMvc.perform(get("/api/package_types"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getPackageTypeById_ok() throws Exception {
+        UUID id = UUID.randomUUID();
+
+        PackageTypeResponseDTO dto = PackageTypeResponseDTO.builder()
+                .id(id.toString())
+                .name("Box")
+                .build();
+
+        when(packageTypeService.getPackageTypeById(id)).thenReturn(dto);
+
+        mockMvc.perform(get("/api/package_types/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Box"));
+    }
+}
