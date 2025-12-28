4440dcbde77f2f2e0bf18e250f9fe4187a8da301
pety02 <petya.licheva2002@abv.bg>
2025-12-28 23:13:26 +0200

    Employee role controller unit tested.

---
+++ src/test/java/com/example/warehouseapp/controller/EmployeeRoleControllerTest.java
@@ -0,0 +1,51 @@
+package com.example.warehouseapp.controller;
+
+import com.example.warehouseapp.model.dto.EmployeeRoleResponseDTO;
+import com.example.warehouseapp.service.EmployeeRoleService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(EmployeeRoleController.class)
+class EmployeeRoleControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private EmployeeRoleService employeeRoleService;
+
+    @Test
+    void getAllEmployeeRoles_ok() throws Exception {
+        when(employeeRoleService.getAllEmployeeRoles()).thenReturn(List.of());
+
+        mockMvc.perform(get("/api/employee_roles"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getEmployeeRoleById_ok() throws Exception {
+        UUID id = UUID.randomUUID();
+
+        EmployeeRoleResponseDTO dto = EmployeeRoleResponseDTO.builder()
+                .id(id.toString())
+                .name("Manager")
+                .build();
+
+        when(employeeRoleService.getEmployeeRoleById(id)).thenReturn(dto);
+
+        mockMvc.perform(get("/api/employee_roles/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Manager"));
+    }
+}
