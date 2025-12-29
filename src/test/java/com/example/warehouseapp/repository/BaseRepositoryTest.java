bc36a78e5625803bcd08d0019e50eb1e8bdd9f98
pety02 <petya.licheva2002@abv.bg>
2025-12-29 11:23:11 +0200

    Base repository unit test implemented.

---
+++ src/test/java/com/example/warehouseapp/repository/BaseRepositoryTest.java
@@ -0,0 +1,9 @@
+package com.example.warehouseapp.repository;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+class BaseRepositoryTest {
+}
