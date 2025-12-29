d1163e7465a5b55c7f2b75d1c2f9012d6a5e68a0
pety02 <petya.licheva2002@abv.bg>
2025-12-29 11:23:25 +0200

    Employee credentials repository unit test implemented.

---
+++ src/test/java/com/example/warehouseapp/repository/EmployeeCredentialsRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.example.warehouseapp.repository;
+
+import com.example.warehouseapp.model.entites.Employee;
+import com.example.warehouseapp.model.entites.EmployeeCredentials;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+class EmployeeCredentialsRepositoryTest {
+
+    @Autowired
+    private EmployeeCredentialsRepository repository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    void findByEmail_success() {
+        EmployeeCredentials creds = repository.save(
+                EmployeeCredentials.builder()
+                        .email("test@mail.com")
+                        .password("pwd")
+                        .build()
+        );
+
+        Optional<EmployeeCredentials> result =
+                repository.findByEmail("test@mail.com");
+
+        assertTrue(result.isPresent());
+        assertEquals(creds.getId(), result.get().getId());
+    }
+
+    @Test
+    void findEmployeeCredentialsByEmployeeId_success() {
+        EmployeeCredentials creds = repository.save(
+                EmployeeCredentials.builder()
+                        .email("x@mail.com")
+                        .password("pwd")
+                        .build()
+        );
+
+        Employee employee = employeeRepository.save(
+                Employee.builder().credentials(creds).isActive(true).build()
+        );
+
+        Optional<EmployeeCredentials> result =
+                repository.findEmployeeCredentialsByEmployeeId(employee.getId());
+
+        assertTrue(result.isPresent());
+    }
+}
